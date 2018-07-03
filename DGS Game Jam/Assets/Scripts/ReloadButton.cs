c6decd918efc1eb765863d5ff4eba2f60cb5fd1f
Lorenzo <sheepytaggernep@gmail.com>
2018-07-03 17:49:54 +0200

    Reload button , BombFireToggle event fixed

---
+++ DGS Game Jam/Assets/Scripts/ReloadButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class ReloadButton : MonoBehaviour
+    {
+        public void OnClick()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
